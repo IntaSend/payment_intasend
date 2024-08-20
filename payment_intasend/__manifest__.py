416dd1ddb1706f5b746891aee13fb09da0ff0aaf
fabulousmochama <fabulousmochama@gmail.com>
2024-08-20 10:19:20 +0300

    Improve module rating: Add cover image and update description to HTML format 106

--- payment_intasend/__manifest__.py
+++ payment_intasend/__manifest__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
@@ -7,6 +8,7 @@
     'sequence': 350,
     'website': 'https://intasend.com',
     'author': 'IntaSend Team',
+    'maintainer': 'IntaSend Support Team',  
     'installable': True,
     'summary': "Payment gateway provider for the Kenyan market. Get M-Pesa, PesaLink, and card payments.",
     'description': "Payment gateway provider for the Kenyan market. Get M-Pesa, PesaLink, and card payments.",
@@ -19,5 +21,8 @@
     'post_init_hook': 'post_init_hook',
     'uninstall_hook': 'uninstall_hook',
     'license': 'LGPL-3',
-    'images': ['static/description/cover_image.jpeg'],  # Add this line
+    'images': ['static/description/cover_image.jpeg'], 
+    'support': 'support@intasend.com',  
+    'application': False,  
+    'auto_install': False,  
 }
